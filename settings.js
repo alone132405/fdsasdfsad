eec6b3bc44f0fc303f35352ec3e34ee50bc09260
alone132405 <129098437+alone132405@users.noreply.github.com>
2025-04-14 11:49:54 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Knight',
+  author: 'Bot',
+  botName: "Konoha Bot",
+  botOwner: 'Konoha Shop', // Your name
+  ownerNumber: '917599318577', //Your number
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.2.4",
+};
+
+module.exports = settings;
